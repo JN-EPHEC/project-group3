5a4958d28f7ff68f0829799dc0e760ff1569f8cc
Brice Loriau <he134681@students.ephec.be>
2025-10-07 15:30:41 +0200

    Modif compo

--- components/BriceHelloJeSuisLeBest.tsx
+++ components/BriceHelloJeSuisLeBest.tsx
@@ -1,1 +1,7 @@
-console.log("Hello, je suis le meilleur !");
+import
+
+const BriceHelloJeSuisLeBest: React.FC = () => {
+    return <h1>Hello, je suis le best!</h1>;
+};
+
+export default BriceHelloJeSuisLeBest; React from 'react';
