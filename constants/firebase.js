58ee3441d9e8864abeb0f00d3e831266907ca0dd
Brice Loriau <he134681@students.ephec.be>
2025-11-18 14:20:44 +0100

    Modif gitignore to exclude firebaseenv.js and update firebase.js to import config from it

--- constants/firebase.js
+++ constants/firebase.js
@@ -19,8 +19,7 @@ import {
   where
 } from 'firebase/firestore';
 
-const firebaseConfig = {  apiKey: "AIzaSyBBw-uc8UozoUhFI2J8hahWGQLd9s3c7gc",  authDomain: "wekid-test.firebaseapp.com",  projectId: "wekid-test",  storageBucket: "wekid-test.firebasestorage.app",  messagingSenderId: "901894988107",  appId: "1:901894988107:web:d24a57f713ba9e1e9488ce"};
-
+import firebaseConfig from './firebaseenv.js';
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
