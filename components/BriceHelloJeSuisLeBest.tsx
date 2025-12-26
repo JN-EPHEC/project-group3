16e76e51bba36900ac9ee7987dbea812247f5381
He134681 <he134681@students.ephec.be>
2025-12-26 10:27:29 +0100

    feat: Remove unused component files for better code maintenance

--- components/BriceHelloJeSuisLeBest.tsx
+++
@@ -1,8 +0,0 @@
-import
-
-const BriceHelloJeSuisLeBest: React.FC = () => {
-    return <h1>Hello, je suis le best!</h1>;
-};
-
-export default BriceHelloJeSuisLeBest; React from 'react';
-//test pour lazlo
