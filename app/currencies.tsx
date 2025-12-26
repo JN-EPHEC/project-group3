25b7c3d12805e564b17a0074aa051a2169cf1074
He134681 <he134681@students.ephec.be>
2025-12-26 13:47:58 +0100

    fix: Reorder imports in currencies and firebase files for consistency

--- app/currencies.tsx
+++ app/currencies.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import CURRENCIES from '../constants/currencies';
 
 export default function CurrenciesScreen() {
